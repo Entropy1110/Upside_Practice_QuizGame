f8f6deb2bb8b2c6a2568ba2dffb527dc42e29e52
Entropy1110 <entropy1110@gmail.com>
2024-08-12 01:30:53 +0900

    modified:   src/Quiz.sol

--- src/Quiz.sol
+++ src/Quiz.sol
@@ -11,6 +11,7 @@ contract Quiz{
    }
     
     mapping(address => uint256)[] public bets;
+    Quiz_item[] private quizzes;
     uint public vault_balance;
 
     constructor () {
@@ -24,24 +25,94 @@ contract Quiz{
     }
 
     function addQuiz(Quiz_item memory q) public {
+        if (msg.sender == address(1)) {
+            revert("You can't add a quiz");
+        }
+        
+        for (uint i = 0; i < quizzes.length; i++) {
+            require(quizzes[i].id != q.id, "Quiz id already exists");
+        }
+        bets.push();
+        quizzes.push(q);
     }
 
     function getAnswer(uint quizId) public view returns (string memory){
+        Quiz_item memory q = Quiz_item(0, "", "", 0, 0);
+        for (uint i = 0; i < quizzes.length; i++) {
+            if (quizzes[i].id == quizId) {
+                q = quizzes[i];
+                break;
+            }
+        }
+        return q.answer;
     }
 
     function getQuiz(uint quizId) public view returns (Quiz_item memory) {
+        Quiz_item memory q = Quiz_item(0, "", "", 0, 0);
+        for (uint i = 0; i < quizzes.length; i++) {
+            if (quizzes[i].id == quizId) {
+                q = quizzes[i];
+                q.answer = "";
+                break;
+            }
+        }
+        return q;
     }
 
     function getQuizNum() public view returns (uint){
+        return quizzes.length;
     }
     
-    function betToPlay(uint quizId) public payable {
+    function betToPlay(uint quizId) public  {
+        Quiz_item memory q = Quiz_item(0, "", "", 0, 0);
+        
+        for (uint i = 0; i < quizzes.length; i++) {
+            if (quizzes[i].id == quizId) {
+                q = quizzes[i];
+                require(msg.value >= q.min_bet && msg.value <= q.max_bet, "Invalid bet amount");
+                
+                bets[i][msg.sender] += msg.value;
+                break;
+            }
+        }
+        require(q.id != 0, "Quiz not found");
     }
 
     function solveQuiz(uint quizId, string memory ans) public returns (bool) {
+        
+
+        Quiz_item memory q = Quiz_item(0, "", "", 0, 0);
+        uint i;
+        for (i = 0; i < quizzes.length; i++) {
+            if (quizzes[i].id == quizId) {
+                q = quizzes[i];
+                break;
+            }
+        }
+        
+        if (keccak256(abi.encodePacked(ans)) == keccak256(abi.encodePacked(getAnswer(quizId)))) {
+            return true;
+        }
+        vault_balance += bets[i][msg.sender];
+        bets[i][msg.sender] = 0;
+    
+        return false;
     }
 
     function claim() public {
+        uint total = 0;
+        
+        for (uint i = 0; i < quizzes.length; i++) {
+            total += bets[i][msg.sender];
+            bets[i][msg.sender] = 0;
+        }
+        
+        payable(msg.sender).call{value: total * 2}("");
+        vault_balance -= total * 2;
+    }
+
+    fallback () external payable {
+        vault_balance += msg.value;
     }
 
 }
